3d91321ddcb6f4a351c64fda0f68f0a07c68745c
Rodrigo Troncoso <rod.tronco@gmail.com>
2015-11-23 17:24:57 -0300

    Adapt ODBCDriverConnector for complex database connection configurations

--- src/Ccovey/ODBCDriver/ODBCDriverConnector.php
+++ src/Ccovey/ODBCDriver/ODBCDriverConnector.php
@@ -1,28 +1,45 @@
-<?php
-
-namespace Ccovey\ODBCDriver;
+<?php namespace Ccovey\ODBCDriver;
 
 use Illuminate\Database\Connectors\Connector;
 use Illuminate\Database\Connectors\ConnectorInterface;
+use PDO;
 
 class ODBCDriverConnector extends Connector implements ConnectorInterface
 {
-	public function connect(array $config)
-	{
-		$dsn = $this->getDsn($config);
-		
+
+    public function connect(array $config)
+    {
+        $dsn = $this->getDsn($config);
+
         $options = $this->getOptions($config);
 
         $connection = $this->createConnection($dsn, $config, $options);
 
         return $connection;
-	}
+    }
 
-	protected function getDsn(array $config) {
+    protected function getDsn(array $config) {
         extract($config);
 
-        $dsn = "odbc:{$dsn}; {$username}; {$password};";
+        $dsn = "odbc:{$dsn}; Server={$host}; Uid={$username}; Pwd={$password}; Database={$database}";
 
         return $dsn;
     }
+
+    /**
+     * Create a new PDO connection.
+     *
+     * @param  string  $dsn
+     * @param  array   $config
+     * @param  array   $options
+     * @return \PDO
+     */
+    public function createConnection($dsn, array $config, array $options)
+    {
+        $username = array_get($config, 'username');
+        $password = array_get($config, 'password');
+
+        return new PDO($dsn, $username, $password, $options);
+    }
+
 }
